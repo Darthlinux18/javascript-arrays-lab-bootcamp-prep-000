22381e2197951d882a1ff25eea6f835f5737835f
craig sperlazza <craig.sperlazza@gmail.com>
2018-01-03 22:47:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,6 +30,6 @@ function prependKitten (element) {
 }
 
 function removeLastKitten () {
-  var newArray = kittens.slice[kittens.length(-1)];
+  var newArray = kittens.slice (0, kittens.length(-1));
   return newArray
 }
