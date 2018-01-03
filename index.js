477ea01112230901afeb4d17b4363f653b75ac7e
craig sperlazza <craig.sperlazza@gmail.com>
2018-01-03 22:40:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ function destructivelyRemoveFirstKitten () {
 }
 
 function appendKitten (name) {
-  var newArray = kittens.push (name);
+  var newArray = kittens (..., name);
   return newArray
 }
