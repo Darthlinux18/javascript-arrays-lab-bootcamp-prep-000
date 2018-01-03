f31fb0780b82e07602cb581367d2de5950338757
craig sperlazza <craig.sperlazza@gmail.com>
2018-01-03 22:33:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten (array, name) {
-  array.push(name);
+  push.array(name);
   return array
 }
